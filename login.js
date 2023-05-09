9fb909e34a63afd57ab9673fb099d87afc413f5d
Eder Assis <109834675+assis1983@users.noreply.github.com>
2023-05-09 13:02:05 -0300

    Altera o tamanho das divs do formulário de cadastro

--- login.js
+++ login.js
@@ -2,12 +2,12 @@ const loginForm = document.getElementById('formulario');
 loginForm.addEventListener('submit', async (event) => {
     event.preventDefault();
     const email = document.getElementById('inputemail').value;
-    const senha = document.getElementById('inputsenha').value;
+    const logsenha = document.getElementById('inputsenha').value;
     const resposta = await fetch ('https://dbjson-service-pacientes.onrender.com/cadastro')
     const cadastro = await resposta.json()
     cadastro.filter(element => {
       if (element.emailCadastro === email) {
-         if (element.senha === senha)
+         if (element.senha === logsenha)
           window.location.href ="cliente.html"
       } else {
         alert('Login ou Senha Inválidos')
